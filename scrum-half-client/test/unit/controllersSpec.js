8d0afc0c20168abc7546fae59c2e31f29cc0164e
joaomiguel.pereira <joaomiguel.pereira@gmail.com>
2012-10-09 18:43:00 +0100

    initial commit

--- scrum-half-client/test/unit/controllersSpec.js
+++ scrum-half-client/test/unit/controllersSpec.js
@@ -29,7 +29,7 @@ describe('Controllers', function () {
 
             //After training responses, flush all pending requests
             $httpBackend.flush();
-            expect(scope.projects.length).toEqual(2);
+            expect(scope.projects.length).toEqual(3);
 
         });
     });
